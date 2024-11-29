e0f8a3f28032cfede955f36240663e4effbdcec9
Hasun2000 <intern@9hermits.com>
2024-11-29 11:11:32 +0530

    Build v0.5 - alpha

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "NineHermitsGames-FuriousSkies-1.0";
 const contentToCache = [
     "Build/0145c752a950e797e3f5c26444736e72.loader.js",
-    "Build/0242539617ddbe1fd0f9845beef811eb.framework.js",
-    "Build/8da615b4136105535f1d82d7e6e2dc9d.data",
-    "Build/c3a0f613d42fabadf47bc2f1dc03b872.wasm",
+    "Build/7e8340ca926a84bae22ea3872307f9b0.framework.js",
+    "Build/4ffaf72e9efffa4b5268f610072fbe48.data",
+    "Build/0226d3b2c5ed631ae45652806466875c.wasm",
     "TemplateData/style.css"
 
 ];
